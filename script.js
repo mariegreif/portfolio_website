c7fc4d3830096e30dbbdf8874598c9e813267759
Ann-Marie Greif <annx9854@stud.kea.dk>
2019-01-04 17:48:17 +0100

    navbar, index, tema1

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+window.addEventListener("load", sidenVises);
 
+function sidenVises() {
+    console.log("siden vises!");
+    //registrer klik på menu knap
+    document.querySelector("#menu_dropdown").addEventListener("click", toggleMenu);
+}
+
+function toggleMenu() {
+    console.log("Toggle menu");
+    document.querySelector("#drop-button").classList.toggle("hidden");
+
+
+    let erSkjult =
+        document.querySelector("#drop-button").classList.contains("hidden");
+
+    if (erSkjult == true) {
+        // menuen er nu skjult - ændre menuknap til lll
+        document.querySelector("#menu_dropdown").textContent = "Menu";
+    } else {
+        // menuen er nu vist - ændre menuknap til X
+        document.querySelector("#menu_dropdown").textContent = "Skjul";
+    }
+}
