9c652d2b92da0e456858684a1246572960f639cc
Ann-Marie Greif <annx9854@stud.kea.dk>
2019-01-07 21:49:49 +0100

    getting there

--- script.js
+++ script.js
@@ -22,3 +22,28 @@ function toggleMenu() {
         document.querySelector("#menu_dropdown").textContent = "Skjul";
     }
 }
+
+
+//var i = 0; // start
+//var images = [];
+//var time = 3000;
+//
+//// Image list
+//images[0] = 'billeder/prototype_fronter.JPG';
+//images[1] = 'billeder/prototype_fronter2.JPG';
+//
+////Change image
+//
+//function changeImg() {
+//    document.slide.src = images[i];
+//
+//    if (i < images.length - 1) {
+//        i++;
+//    } else {
+//        i = 0;
+//    }
+//
+//    setTimeout("changeImg()", time);
+//}
+//
+//window.onload = changeImg;
